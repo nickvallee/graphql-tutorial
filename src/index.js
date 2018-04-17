853a352aedb52be60420b68a4f57d7cc709f4a5e
NICK VALLEE <nickvallee@gmail.com>
2018-04-17 17:45:38 -0400

    add prisma db

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
+        feed: () => (root, args, context, info) => {
+            return context.db.query.links({}, info)
+        },
         link (obj, args) {
             return links.find((link) =>  link.id === args.id );
         },
     },
     Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-            links.push(link)
-            return link
-        },
-        updateLink: function (root, args ) {
-            console.log("ROOT", root, "ARGS", args)
-
-            if (!links[args.id]) {
-                throw new Error('no message exists with id ' + id);
-            }
-            // This replaces all old data, but some apps might want partial update.
-            return new Message(id, input);
+        post: (root, args, context, info) => {
+            return context.db.mutation.createLink({
+                data: {
+                  url: args.url,
+                  description: args.description,
+                },
+            }, info)
         },
     },
 
@@ -43,5 +29,14 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://eu1.prisma.sh/public-mangovole-825/hackernews-node/dev',
+            secret: 'mysecret123',
+            debug: true,
+        }),
+    }),
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
